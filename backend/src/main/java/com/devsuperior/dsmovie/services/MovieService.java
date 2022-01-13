71ec861f66f437917bb454ab7fb45635eb367da8
Luiz Felipe Vieira Cordeiro <luizfelipevcordeiro@hotmail.com>
2022-01-13 14:53:32 -0300

    Save score

--- backend/src/main/java/com/devsuperior/dsmovie/services/MovieService.java
+++ backend/src/main/java/com/devsuperior/dsmovie/services/MovieService.java
@@ -1,7 +1,5 @@
 package com.devsuperior.dsmovie.services;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -14,22 +12,24 @@ import com.devsuperior.dsmovie.repositories.MovieRepository;
 
 @Service
 public class MovieService {
-	@Autowired
-	private MovieRepository repository;
 	
-	@Transactional(readOnly = true)
-	public Page<MovieDTO> findAll(Pageable pageable){
-		Page<Movie> result = repository.findAll(pageable);
-		Page<MovieDTO> page= result.map(x -> new MovieDTO(x));
-		return page;
-	}
-	
-	@Transactional(readOnly = true)
-	public MovieDTO findById(Long id) {
-		Movie result = repository.findById(id).get();
-		MovieDTO dto = new MovieDTO(result);
-		return dto;
-	}
-	
-	
+		@Autowired
+		private MovieRepository repository;
+		
+		@Transactional(readOnly = true)
+		public Page<MovieDTO> findAll(Pageable pageable){
+			Page<Movie> result = repository.findAll(pageable);
+			Page<MovieDTO> page= result.map(x -> new MovieDTO(x));
+			return page;
+		}
+		
+		@Transactional(readOnly = true)
+		public MovieDTO findById(Long id) {
+			Movie result = repository.findById(id).get();
+			MovieDTO dto = new MovieDTO(result);
+			return dto;
+		}
+		
+		
 }
+
