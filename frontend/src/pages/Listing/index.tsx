f26b1befc8d382f91e7495d6b09a2c35f91887a7
Luiz Felipe Vieira Cordeiro <luizfelipevcordeiro@hotmail.com>
2022-01-14 15:43:54 -0300

    Props

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -6,6 +6,13 @@ import { useEffect, useState } from 'react';
 import { MoviePage } from 'types/movie';
 function Listing() {
 
+    const movie = {
+        "id": 1,
+        "title": "The Witcher",
+        "score": 4.5,
+        "count": 2,
+        "image": "https://www.themoviedb.org/t/p/w533_and_h300_bestv2/jBJWaqoSCiARWtfV0GlqHrcdidd.jpg"
+    };
 
     //Uso do hook state
 
@@ -21,38 +28,40 @@ function Listing() {
     * useEffect permite que a requisição axios seja feita apenas 1 vez, quando o site é aberto
     * o primeiro argumento é uma função e o segundo é uma coleção de objetos que são monitorados
     */
+    //Estado para guardar a página de requisição
+    const [page, setPage] = useState<MoviePage>({
+        content: [],
+        last: true,
+        totalPages: 0,
+        totalElements: 0,
+        size: 12,
+        number: 0,
+        first: true,
+        numberOfElements: 0,
+        empty: true,
+    });
+
     useEffect(() => {
-        axios.get(`${BASE_URL}/movies?size=12&page=1`)
+        axios.get(`${BASE_URL}/movies?size=12&page=${pageNumber}`)
             .then(response => {
                 const data = response.data as MoviePage;
-                console.log(data);
-                setPageNumber(data.number);
-                
+                setPage(data);
             });
-    }, [])
+    }, [pageNumber])
 
 
 
     return (
         <>
-            <p>{pageNumber}</p>
             <div className='container'>
                 <div className='row'>
-                    <div className='col-sm-6 col-lg-4 col-xl-3 col-xxl-2 mb-3'>
-                        <MovieCard />
-                    </div>
-                    <div className='col-sm-6 col-lg-4 col-xl-3 col-xxl-2 mb-3'>
-                        <MovieCard />
-                    </div>
-                    <div className='col-sm-6 col-lg-4 col-xl-3 col-xxl-2 mb-3'>
-                        <MovieCard />
-                    </div>
-                    <div className='col-sm-6 col-lg-4 col-xl-3 col-xxl-2 mb-3'>
-                        <MovieCard />
-                    </div>
-                    <div className='col-sm-6 col-lg-4 col-xl-3 col-xxl-2 mb-3'>
-                        <MovieCard />
-                    </div>
+                    {page.content.map(movie => (
+                        //Sempre que houver renderização dinâmica de uma coleção de objeto, é necessário o atributo key de valor único
+                        <div key = {movie.id} className='col-sm-6 col-lg-4 col-xl-3 col-xxl-2 mb-3'>
+                            <MovieCard movie={movie} />
+                        </div>
+                    ))}
+
                 </div>
             </div>
             <Pagination />
