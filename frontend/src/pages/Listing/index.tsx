bdc7945c1de48c1db899b840429bfd266cc0a9be
Luiz Felipe Vieira Cordeiro <luizfelipevcordeiro@hotmail.com>
2022-01-14 13:11:57 -0300

    First request

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,6 +1,19 @@
 import Pagination from 'components/Pagination';
 import MovieCard from 'components/MovieCard';
+import axios from 'axios';
+import { BASE_URL } from 'utils/requests';
 function Listing() {
+
+    //Forma errada
+    
+    /*
+    A biblioteca axios permite fazer requisições http de maneira simples
+    */
+    axios.get(`${BASE_URL}/movies?size=12&page=0`)
+        .then(response => {
+            console.log(response.data);
+        });
+
     return (
         <>
             <div className='container'>
