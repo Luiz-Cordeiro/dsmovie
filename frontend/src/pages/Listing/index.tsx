753f2d1b1262892ace319964510872d5467f1529
Luiz Felipe Vieira Cordeiro <luizfelipevcordeiro@hotmail.com>
2022-01-14 17:03:05 -0300

    Pagination

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -49,10 +49,14 @@ function Listing() {
             });
     }, [pageNumber])
 
-
+    const handlePageChange = (newPageNumber: number) => {
+        setPageNumber(newPageNumber);
+    }
 
     return (
         <>
+            <Pagination page={page} onChange={handlePageChange}/>
+
             <div className='container'>
                 <div className='row'>
                     {page.content.map(movie => (
@@ -64,7 +68,7 @@ function Listing() {
 
                 </div>
             </div>
-            <Pagination />
+            
         </>
     );
 }
